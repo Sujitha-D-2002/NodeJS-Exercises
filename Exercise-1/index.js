1a9ebdb589d4d03d68296b6826033ffd1eca9450
Sujitha-D-2002 <sujitha.dhamotharan@cdw.com>
2023-04-03 18:44:13 +0530

    Color_Palette-Exercise-1

--- Exercise-1/index.js
+++ Exercise-1/index.js
@@ -1,29 +1,63 @@
-let fs = require('fs');
-
-let colorCodeFile = "./color_ palette.json";
+const fs = require('fs');
+const fromJSON = undefined;
+const colorCodeFile = "./color_ palette.json";
+let randomNumArray;
 //Store the attached `color_ palette.json` in your filesystem
-let colorCodes = fs.readFileSync(colorCodeFile, 'utf-8');
+const colorCodes = function async(colorCodeFile) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(colorCodeFile, 'utf-8', function (err, data) {
+            if (err) {
+                reject("File not found..!!!");
+            }
+            else {
+                resolve(data);
+            }
+        });
+    }).then(data => {
+        return data;
+    }).catch(err => {
+        console.log(err);
+    })
+}
+colorPickingFunction();
+async function colorPickingFunction() {
+    let randomColorCodes = await colorCodes(colorCodeFile);
+    if (randomColorCodes == '[]') {
+        console.log("No data..!!!");
+    }
+    else {
+        randomColorCode = JSON.parse(randomColorCodes);
+        if (randomColorCode.length < 5) {
+            console.log("Less data found..!!!");
+        }
+        else {
+            randomNumArray = [0, 1, 2, 3, 4];
+            randomColorPaletteGenerate(randomColorCode);
+            const data = JSON.stringify(randomColorCode.slice(0, 5));
+            // write the randomized 5 colors into json file
+            fs.writeFileSync("randomized_color_ palette.json", data);
+            //Read the newly created `randomized_color_ palette.json`
+            try {
+                let color = fs.readFileSync("randomized_color_ palette.json", "utf8");
+                console.log(color);
+            }
+            catch {
+                console.log("File not found to write data..!!");
+            }
+        }
+    }
 
-randomColorCodes = randomColorPaletteGenerate(JSON.parse(colorCodes));
 
+}
 //Randomize the color palette and take 5 colors
 function randomColorPaletteGenerate(arrayColorPalette) {
-    for (let i = arrayColorPalette.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [arrayColorPalette[i], arrayColorPalette[j]] = [arrayColorPalette[j], arrayColorPalette[i]];
+    for (let i = arrayColorPalette.length - 1, j = 0; i > 0 && j < 5; i--, j++) {
+        k = Math.floor(Math.random() * (i + 1));
+        randomNumArray[j] = k;
+    }
+    for (let i = 0; i < 5; i++) {
+        arrayColorPalette[i] = arrayColorPalette[randomNumArray[i]];
     }
     return arrayColorPalette;
 }
-console.log(randomColorCodes.slice(0, 5));
-let data = JSON.stringify(randomColorCodes.slice(0, 5));
 
-// write the randomized 5 colors into json file
-fs.writeFile("randomized_color_ palette.json", data, (err) => {
-    if (err)
-        console.log(err);
-    else {
-        console.log("File written successfully\n");
-        //Read the newly created `randomized_color_ palette.json`
-        console.log(fs.readFileSync("randomized_color_ palette.json", "utf8"));
-    }
-});
